0413fe8f1b896b313583369a50172d2c2fe994a7
Yeru0 <yeru065077@gmail.com>
2026-03-18 23:13:13 +0100

    Finishing touches

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,20 +4,47 @@
 
 
 <svelte:head>
-	<title>Ez itt a fo oldal</title>
+	<title>Élien: Főoldal</title>
 </svelte:head>
 
-<section>
-    <h2>Üdvözli az</h2>
-    
-    <h1>Élilyen Kommunikációs Diválysz™</h1>
+<section>    
+    <h1>Üdvözli az Élilyen Kommunikációs Diválysz™</h1>
     
     <p>Mi az az Élilyen Kommunikációs Diválysz™? Az Élilyen Kommunikációs Diválysz™ lehetővé teszi a használók...</p>
 </section>
 
 
-<section>
+<section class="t">
     <h2>Felkeltette az érdeklődését?</h2>
     
     <a href="/divalysz">Tovább az Élilyen Kommunikációs Diválysz™-hoz</a>
 </section>
+
+
+<style>
+    .t {
+        display: grid;
+    }
+
+    a {
+        width: min-content;
+        margin: auto;
+        color: inherit;
+        font-weight: bold;
+        transition: .3s all ease-in-out;
+		border: 1px solid transparent;
+		cursor: pointer;
+		padding: 12px 24px;
+        background-color: #333;
+		border-radius: 6px;
+        width: fit-content;
+        place-self: center;
+        text-decoration: none;
+
+        &:hover {
+	    	color: #fff;
+	    	background-color: #000;
+	    	border: 1px solid #ffea00;
+	    }
+    }
+</style>
